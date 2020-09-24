69bc3faf73845a11b58c1635c8b0659823bd1a28
Valentin <kibirevvalik202@gmail.com>
2020-09-24 14:45:07 +0300

    [update] scripts

--- SoftBodyDemo/Assets/Scripts/Blob.cs
+++ SoftBodyDemo/Assets/Scripts/Blob.cs
@@ -1,23 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Blob : MonoBehaviour
 {
-    ////private class PropagateCollisions : MonoBehaviour
-    ////{
-    ////    void OnCollisionEnter2D(Collision2D collision)
-    ////    {
-    ////        transform.parent.SendMessage("OnCollisionEnter2D", collision);
-    ////    }
-    ////}
     [SerializeField]
-    private int _width = 5, _height = 5,_referencePointsCount = 12;
+    private int _width = 5, _height = 5/*, _referencePointsCount = 12*/;
     [SerializeField]
     private float _sizeCollider, _step, _mappingDetail = 10, _springDampingRatio = 0, _springFrequency = 2;
     [SerializeField]
     private PhysicsMaterial2D _surfaceMaterial;
+    [SerializeField]
+    private MeshFilter _meshMain;
+    [SerializeField]
+    private Rigidbody2D _rbMain;
 
-    private GameObject[] _referencePoints;
+    private List<GameObject> _referencePoints;
     private Vector3[] _vertices;
     private Vector2[] _uv;
     private Vector3[,] _offsets;
@@ -38,108 +36,187 @@ public class Blob : MonoBehaviour
     }
     private void CreateReferencePoints()
     {
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-
-        _referencePointsCount = Mathf.RoundToInt(transform.localScale.x / _step);
-
-        _referencePoints = new GameObject[_referencePointsCount];
-
-        float positionCollider = -(_sizeCollider / 2 + (_sizeCollider * (Mathf.Round(transform.localScale.x) - 1f)));
-
-        for (int i = 0; i < _referencePointsCount; i++)
+        if (_surfaceMaterial == null)
         {
-            _referencePoints[i] = new GameObject();
+            Debug.LogError("lack of physical material");
+        }
+        _referencePoints = new List<GameObject>();
+
+        float positionColliderX = transform.position.x - (transform.localScale.x / 2 - _sizeCollider / 2);
+        float positionColliderY = transform.position.y + (transform.localScale.y / 2 - _sizeCollider / 2);
+        Vector2 positionCollider = new Vector2(positionColliderX, positionColliderY);
+        bool EmergencyExit = false;
+        bool attach = false;
+
+        int i = 0;
+        int actionNumber = 0;
+
+        while (true)
+        {
+            _referencePoints.Add(new GameObject());
             _referencePoints[i].tag = gameObject.tag;
-            //_referencePoints[i].AddComponent<PropagateCollisions>();
             _referencePoints[i].transform.parent = transform;
 
-            #region Old
-            //переработать
-            //Quaternion rotation =
-            //    Quaternion.AngleAxis(angle * (i - 1), Vector3.back);
-            //referencePoints[i].transform.localPosition =
-            //    rotation * offsetFromCenter; 
-            #endregion
-
-            _referencePoints[i].transform.position = new Vector2(transform.position.x + positionCollider, transform.position.y + 0.25f);
-
-            positionCollider += _step;
-
-            Rigidbody2D body = _referencePoints[i].AddComponent<Rigidbody2D>();
-            body.freezeRotation = true;
-            body.interpolation = rigidbody.interpolation;
-            body.collisionDetectionMode = rigidbody.collisionDetectionMode;
-
-            BoxCollider2D collider =
-                _referencePoints[i].AddComponent<BoxCollider2D>();
-
-            collider.size = new Vector2(_sizeCollider, _sizeCollider);
-
-            if (_surfaceMaterial != null)
+            switch (actionNumber)
             {
-                collider.sharedMaterial = _surfaceMaterial;
+                case 0:
+                    if (positionCollider.x > transform.position.x + (transform.localScale.x / 2 - _sizeCollider / 2))
+                    {
+                        positionCollider.x = transform.position.x + (transform.localScale.x / 2 - _sizeCollider / 2);
+                        actionNumber += 1;
+                        attach = true;
+                    }
+                    break;
+
+                case 1:
+                    if (positionCollider.y < transform.position.y - (transform.localScale.y / 2 - _sizeCollider / 2))
+                    {
+                        positionCollider.y = transform.position.y - (transform.localScale.y / 2 - _sizeCollider / 2);
+                        actionNumber += 1;
+                        attach = true;
+                    }
+                    break;
+
+                case 2:
+                    if (positionCollider.x < transform.position.x - (transform.localScale.x / 2 - _sizeCollider / 2))
+                    {
+                        positionCollider.x = transform.position.x - (transform.localScale.x / 2 - _sizeCollider / 2);
+                        actionNumber += 1;
+                        attach = true;
+                    }
+                    break;
+
+                case 3:
+                    if (positionCollider.y > transform.position.y + (transform.localScale.y / 2 - _sizeCollider / 2))
+                    {
+                        positionCollider.y = transform.position.y + (transform.localScale.y / 2 - _sizeCollider / 2);
+                        EmergencyExit = true;
+                    }
+                    break;
             }
-            AttachWithSpringJoint(_referencePoints[i], gameObject);
+
+            _referencePoints[i].transform.position = positionCollider;
+
+            switch (actionNumber)
+            {
+                case 0:
+                    positionCollider.x += _step;
+                    break;
+
+                case 1:
+                    positionCollider.y -= _step;
+                    break;
+
+                case 2:
+                    positionCollider.x -= _step;
+                    break;
+
+                case 3:
+                    positionCollider.y += _step;
+                    break;
+            }
+
+            AddComponents(_referencePoints[i]);
+
+            AttachWithSpringJoint(_referencePoints[i], gameObject, _springFrequency);
+
             if (i > 0)
             {
                 AttachWithSpringJoint(_referencePoints[i],
-                        _referencePoints[i - 1]);
+                        _referencePoints[i - 1], _springFrequency * 2);
             }
+            else
+            {
+                AttachWithSpringJointToTheFastener(_referencePoints[0],  _springFrequency * 2);
+            }
+
+            if (attach)
+            {
+                AttachWithSpringJointToTheFastener(_referencePoints[i], _springFrequency * 2);
+                attach = false;
+            }
+            if (EmergencyExit)
+            {
+                AttachWithSpringJoint(_referencePoints[0],
+                        _referencePoints[_referencePoints.Count - 1], _springFrequency * 2);
+
+                break;
+            }
+
+            i++;
         }
-        AttachWithSpringJoint(_referencePoints[0], true);
-        AttachWithSpringJoint(_referencePoints[_referencePoints.Length-1], false);
+
         IgnoreCollisionsBetweenReferencePoints();
     }
-    private void AttachWithSpringJoint(GameObject referencePoint, GameObject connected)
+    private void AddComponents(GameObject referencePoints)
     {
-        SpringJoint2D springJoint =
-            referencePoint.AddComponent<SpringJoint2D>();
+        Rigidbody2D body = referencePoints.AddComponent<Rigidbody2D>();
+        body.freezeRotation = true;
+        body.interpolation = _rbMain.interpolation;
+        body.collisionDetectionMode = _rbMain.collisionDetectionMode;
+
+        CircleCollider2D collider = referencePoints.AddComponent<CircleCollider2D>();
+        collider.radius = _sizeCollider / 2;
+        collider.sharedMaterial = _surfaceMaterial;
+    }
+    private void AttachWithSpringJoint(GameObject referencePoint, GameObject connected, float springFrequency)
+    {
+        SpringJoint2D springJoint = referencePoint.AddComponent<SpringJoint2D>();
         springJoint.connectedBody = connected.GetComponent<Rigidbody2D>();
         springJoint.connectedAnchor = LocalPosition(referencePoint) -
             LocalPosition(connected);
 
         springJoint.distance = 0;
         springJoint.dampingRatio = _springDampingRatio;
-        springJoint.frequency = _springFrequency;
+        springJoint.frequency = springFrequency;
     }
-    private void AttachWithSpringJoint(GameObject referencePoint, bool fastenersFromTheBack)
+    private void AttachWithSpringJointToTheFastener(GameObject referencePoint, float springFrequency)
     {
+        bool topMount = transform.position.y < referencePoint.transform.position.y;
+        bool rearMount = transform.position.x > referencePoint.transform.position.x;
         SpringJoint2D springJoint = referencePoint.AddComponent<SpringJoint2D>();
 
-        Rigidbody2D rbConnected = Fasteners(fastenersFromTheBack);
-        springJoint.connectedBody = rbConnected;
+        Rigidbody2D rbConnected = Fasteners(rearMount, topMount);
 
+        springJoint.connectedBody = rbConnected;
         springJoint.connectedAnchor = LocalPosition(referencePoint) -
             LocalPosition(rbConnected.gameObject);
         springJoint.distance = 0;
         springJoint.dampingRatio = _springDampingRatio;
-        springJoint.frequency = _springFrequency;
+        springJoint.frequency = springFrequency;
     }
-    private Rigidbody2D Fasteners(bool multiplierGreaterThanZero)
+    private Rigidbody2D Fasteners(bool rearMount, bool topMount)
     {
-        int multiplier = multiplierGreaterThanZero ? -1 : 1;
-        float positionCollider = multiplier*(_sizeCollider / 2 + (_sizeCollider * (Mathf.Round(transform.localScale.x) - 1f)));
+        int multiplierX = rearMount ? -1 : 1;
+        int multiplierY = topMount ? 1 : -1;
+
+        float positionColliderX = transform.position.x + ((transform.localScale.x / 2 - _sizeCollider / 2) * multiplierX);
+        float positionColliderY = transform.position.y + ((transform.localScale.y / 2 - _sizeCollider / 2) * multiplierY);
+        Vector2 positionCollider = new Vector2(positionColliderX, positionColliderY);
+
         GameObject fasteners = new GameObject();
 
         fasteners.transform.parent = transform;
-        fasteners.transform.position = new Vector2(transform.position.x + positionCollider+(_step*multiplier), transform.position.y + 0.25f);
+        fasteners.transform.position =
+            new Vector2(positionCollider.x + (_step * multiplierX), positionCollider.y);
 
         Rigidbody2D rbFasteners = fasteners.AddComponent<Rigidbody2D>();
         rbFasteners.freezeRotation = true;
-        rbFasteners.constraints =RigidbodyConstraints2D.FreezePositionY|RigidbodyConstraints2D.FreezePositionX;
+        rbFasteners.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezePositionX;
+
         return rbFasteners;
     }
     private void IgnoreCollisionsBetweenReferencePoints()
     {
-        BoxCollider2D a;
-        BoxCollider2D b;
+        CircleCollider2D a;
+        CircleCollider2D b;
 
-        for (int i = 0; i < _referencePointsCount; i++)
+        for (int i = 0; i < _referencePoints.Count; i++)
         {
-            for (int j = i; j < _referencePointsCount; j++)
+            for (int j = i; j < _referencePoints.Count; j++)
             {
-                a = _referencePoints[i].GetComponent<BoxCollider2D>();
-                b = _referencePoints[j].GetComponent<BoxCollider2D>();
+                a = _referencePoints[i].GetComponent<CircleCollider2D>();
+                b = _referencePoints[j].GetComponent<CircleCollider2D>();
                 Physics2D.IgnoreCollision(a, b, true);
             }
         }
@@ -161,12 +238,12 @@ public class Blob : MonoBehaviour
                 int v = (_width + 1) * y + x;
                 _vertices[v] = new Vector3(x / (float)_width - 0.5f,
                         y / (float)_height - 0.5f, 0);
+
                 _uv[v] = new Vector2(x / (float)_width, y / (float)_height);
 
                 if (x < _width && y < _height)
                 {
                     t = 3 * (2 * _width * y + 2 * x);
-
                     _triangles[t] = v;
                     _triangles[++t] = v + _width + 1;
                     _triangles[++t] = v + _width + 2;
@@ -176,8 +253,7 @@ public class Blob : MonoBehaviour
                 }
             }
         }
-        //тут
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Mesh mesh = _meshMain.mesh;
 
         mesh.vertices = _vertices;
         mesh.uv = _uv;
@@ -185,22 +261,21 @@ public class Blob : MonoBehaviour
     }
     private void MapVerticesToReferencePoints()
     {
-        _offsets = new Vector3[_vertexCount, _referencePointsCount];
-        _weights = new float[_vertexCount, _referencePointsCount];
+        _offsets = new Vector3[_vertexCount, _referencePoints.Count];
+        _weights = new float[_vertexCount, _referencePoints.Count];
 
         for (int i = 0; i < _vertexCount; i++)
         {
             float totalWeight = 0;
 
-            for (int j = 0; j < _referencePointsCount; j++)
+            for (int j = 0; j < _referencePoints.Count; j++)
             {
                 _offsets[i, j] = _vertices[i] - LocalPosition(_referencePoints[j]);
-                _weights[i, j] =
-                    1 / Mathf.Pow(_offsets[i, j].magnitude, _mappingDetail);
+                _weights[i, j] = 1 / Mathf.Pow(_offsets[i, j].magnitude, _mappingDetail);
                 totalWeight += _weights[i, j];
             }
 
-            for (int j = 0; j < _referencePointsCount; j++)
+            for (int j = 0; j < _referencePoints.Count; j++)
             {
                 _weights[i, j] /= totalWeight;
             }
@@ -214,14 +289,14 @@ public class Blob : MonoBehaviour
         {
             vertices[i] = Vector3.zero;
 
-            for (int j = 0; j < _referencePointsCount; j++)
+            for (int j = 0; j < _referencePoints.Count; j++)
             {
                 vertices[i] += _weights[i, j] *
                     (LocalPosition(_referencePoints[j]) + _offsets[i, j]);
             }
         }
 
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Mesh mesh = _meshMain.mesh;
         mesh.vertices = vertices;
         mesh.RecalculateBounds();
     }
---
+++ SoftBodyDemo/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rb;
+    private Vector3 _startMousePos, _currentMousePos, _startPositionPlayer, _velosity;
+    private Camera _cam;
+
+    [SerializeField]
+    private float _maxAddForse, _forceMultiplier;
+
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        _startMousePos = _cam.ScreenToViewportPoint(Input.mousePosition);
+    //        _rb.constraints = RigidbodyConstraints2D.FreezePosition;
+    //        _startPositionPlayer = transform.position;
+    //    }
+    //    else if (Input.GetMouseButton(0))
+    //    {
+    //        if (_startMousePos == Vector3.zero)
+    //        {
+    //            _startMousePos = _cam.ScreenToViewportPoint(Input.mousePosition);
+    //            _rb.constraints = RigidbodyConstraints2D.FreezePosition;
+    //            _startPositionPlayer = transform.position;
+    //        }
+    //        _currentMousePos = _cam.ScreenToViewportPoint(Input.mousePosition);
+    //        if (true)
+    //        {
+
+    //        }
+    //        float Y = _startPositionPlayer.y + ((_currentMousePos - _startMousePos).y) * 2f;
+    //        transform.position = new Vector3(transform.position.x, Y);
+    //    }
+    //    else if (Input.GetMouseButtonUp(0))
+    //    {
+    //        _rb.constraints = RigidbodyConstraints2D.None;
+    //    }
+    //}  
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _rb.constraints = RigidbodyConstraints2D.FreezePosition;
+            transform.position = _cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _rb.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+}
